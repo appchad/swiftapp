12f2417e2fb146af8e413060c7debc26e0398ab8
Chad Willis <>
2018-02-28 22:41:48 -0600

    we can add

--- ViewController.swift
+++ ViewController.swift
@@ -4,12 +4,28 @@
 //
 //  Created by Chad Willis on 2/27/18.
 //  Copyright © 2018 cmw. All rights reserved.
-// going to try to save this commit
 
 import UIKit
 
 class ViewController: UIViewController {
 
+	@IBOutlet weak var Label1: UILabel!
+	
+	
+	@IBOutlet weak var text1: UITextField!
+	
+	@IBOutlet weak var text2: UITextField!
+	
+	@IBAction func button(_ sender: Any) {
+		
+		
+		Label1.text = "Answer: \(Double(text1.text!)! + Double(text2.text!)!)"
+		print(text1.text!)
+		print(text2.text!)
+	}
+	
+	
+	
 	override func viewDidLoad() {
 		super.viewDidLoad()
 		// Do any additional setup after loading the view, typically from a nib.
